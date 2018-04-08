4e14f41a4d6d1aacf07a0d422046984ea3c87c46
Oleg Legun <oleg.legun@gmail.com>
2018-04-08 22:09:19 +0300

    update tests

--- test/polly-text-split.js
+++ test/polly-text-split.js
@@ -1,32 +1,72 @@
 const t = require('tap')
 const textSplit = require('../lib/polly-text-split')
+const { ValidationError, SSMLParseError } = require('../lib/errors')
 
-const options = {
+/*-----------------------------------------------------------------------------
+ *  Tests for configuration validation
+ *----------------------------------------------------------------------------*/
+
+t.throws(function() {
+    textSplit.configure()
+}, ValidationError)
+
+t.throws(function() {
+    textSplit.configure('')
+}, ValidationError)
+
+/*-----------------------------------------------------------------------------
+ *  Tests for splitting plain text
+ *----------------------------------------------------------------------------*/
+
+const defaultOptions = {
     softLimit: 10,
     hardLimit: 15,
 }
 
 const testCasesText = [
     {
-        options,
+        options: defaultOptions,
+        text: 'Simple text.',
+        result: ['Simple text.'],
+    },
+    {
+        options: defaultOptions,
         text: 'Simple text. Simple text.',
         result: ['Simple text.', ' Simple text.'],
     },
     {
+        options: defaultOptions,
+        text: 'Simple text. Simple text. Simple text.',
+        result: ['Simple text.', ' Simple text.', ' Simple text.'],
+    },
+    {
+        options: defaultOptions,
         text: 'Simple text, simple text.',
         result: ['Simple text,', ' simple text.'],
     },
     {
+        options: defaultOptions,        
         text: 'Simple text; simple text.',
         result: ['Simple text;', ' simple text.'],
     },
     {
+        options: defaultOptions,        
         text: 'Simple text simple text.',
         result: ['Simple text', 'simple text.'],
     },
     {
+        options: defaultOptions,        
         text: 'Simple textsimple text.',
         result: ['Simple text', 'simple text.'],
+    },
+    {
+        options: {
+            softLimit: 9,
+            hardLimit: 15,
+            extraSplitChars: ':-',
+        },
+        text: 'First part: second part - third part',
+        result: ['First part:', ' second part -', ' third part'],
     },
 ]
 
@@ -37,3 +77,9 @@ testCasesText.forEach(test => {
 
     t.same(textSplit.splitText(test.text), test.result)
 })
+
+/*-----------------------------------------------------------------------------
+ *  Tests for splitting text with SSML tags
+ *----------------------------------------------------------------------------*/
+
+const testCasesSSML = [{}]
