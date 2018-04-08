d6e30e698059062cbe951104c3636bccac25d1bb
Oleg Legun <oleg.legun@gmail.com>
2018-04-08 21:10:29 +0300

    add tests

--- test/polly-text-split.js
+++ test/polly-text-split.js
@@ -1,6 +1,39 @@
 const t = require('tap')
-const pollyTextSplit = require('../lib/polly-text-split')
+const textSplit = require('../lib/polly-text-split')
 
-const testData = [
-    
+const options = {
+    softLimit: 10,
+    hardLimit: 15,
+}
+
+const testCasesText = [
+    {
+        options,
+        text: 'Simple text. Simple text.',
+        result: ['Simple text.', ' Simple text.'],
+    },
+    {
+        text: 'Simple text, simple text.',
+        result: ['Simple text,', ' simple text.'],
+    },
+    {
+        text: 'Simple text; simple text.',
+        result: ['Simple text;', ' simple text.'],
+    },
+    {
+        text: 'Simple text simple text.',
+        result: ['Simple text', 'simple text.'],
+    },
+    {
+        text: 'Simple textsimple text.',
+        result: ['Simple text', 'simple text.'],
+    },
 ]
+
+testCasesText.forEach(test => {
+    if (test.options) {
+        textSplit.configure(test.options)
+    }
+
+    t.same(textSplit.splitText(test.text), test.result)
+})
